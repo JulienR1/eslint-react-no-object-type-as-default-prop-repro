d1510edb934dde43ae0b6283d48b115451681831
Julien Rousseau <julrousseau20@gmail.com>
2024-06-12 09:54:16 -0400

    udpated eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,8 +4,6 @@ module.exports = {
   extends: ["plugin:react/recommended"],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
   rules: {
     "react/no-object-type-as-default-prop": "error",
     "react/prop-types": "off",
